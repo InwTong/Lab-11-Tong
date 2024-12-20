9fed72a9aa807bc294783c74a6fc66a6761bde82
InwTong <Tachin_s@cmu.ac.th>
2024-12-20 14:41:41 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,26 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    
+    cout << "Press Enter 3 times to reveal your future." << endl;
+    
+    cin.get(); // กด Enter ครั้งที่ 1
+    cin.get(); // กด Enter ครั้งที่ 2
+    cin.get(); // กด Enter ครั้งที่ 3
+
+  
+    srand(static_cast<unsigned int>(time(0)));
+
+  
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    int randomIndex = rand() % 9; // เลือกค่าแบบสุ่มจาก 0 ถึง 8
+
+
+    cout << "You will get " << grades[randomIndex] << " in this 261102." << endl;
+
+    return 0;
+}
